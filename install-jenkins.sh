9aede66057fde9351e7b531406d7c7f7274b9cf4
Karthik Bhat <karthik.bhat@news.co.uk>
2018-02-14 18:14:03 +0530

    Jenkins Install script

---
+++ install-jenkins.sh
@@ -0,0 +1,25 @@
+
+# Install Jenkins
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get -y install jenkins
+
+# Install `Docker Community Edition` required by Jenkins
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce
+
+# Giving Jenkins permission to use docker
+# Add Jenkins user to docker group
+sudo usermod -aG docker $USER
+sudo usermod -aG docker jenkins
