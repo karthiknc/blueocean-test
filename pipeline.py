5b9065b21b48e2b80e59fd8c9b55e589a3c43d35
Karthik Bhat <karthik.bhat@news.co.uk>
2018-02-21 20:24:33 +0530

    test

--- pipeline.py
+++ pipeline.py
@@ -3,7 +3,7 @@ import boto3
 
 s3 = boto3.client('s3')
 print("s3 waiters:")
-s3.waiter_names
+print(s3.waiter_names)
 
 client = boto3.client('codebuild')
 
@@ -11,26 +11,26 @@ print("codebuild waiters:")
 client.waiter_names
 print(client.waiter_names)
 
-response = client.start_build(
-    projectName='nu-ecsplatform-orchestrator',
-    environmentVariablesOverride=[
-        {
-            'name': 'WORKFLOW',
-            'value': os.environ['WORKFLOW']
-        },
-        {
-            'name': 'SITE',
-            'value': os.environ['GIT_URL'].split('/')[-1].split('.')[0]
-        },
-        {
-            'name': 'SITE_BRANCH',
-            'value': os.environ['BRANCH']
-        },
-        {
-            'name': 'BUILD_ENV',
-            'value': os.environ['BUILD_ENV']
-        }
-    ]
-)
+# response = client.start_build(
+#     projectName='nu-ecsplatform-orchestrator',
+#     environmentVariablesOverride=[
+#         {
+#             'name': 'WORKFLOW',
+#             'value': os.environ['WORKFLOW']
+#         },
+#         {
+#             'name': 'SITE',
+#             'value': os.environ['GIT_URL'].split('/')[-1].split('.')[0]
+#         },
+#         {
+#             'name': 'SITE_BRANCH',
+#             'value': os.environ['BRANCH']
+#         },
+#         {
+#             'name': 'BUILD_ENV',
+#             'value': os.environ['BUILD_ENV']
+#         }
+#     ]
+# )
 
-print(response)
+# print(response)
