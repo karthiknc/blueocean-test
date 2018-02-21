d1b4d6c88dfa453d288911bcc6c8de27069958ed
Karthik Bhat <karthik.bhat@news.co.uk>
2018-02-21 19:01:20 +0530

    test

--- pipeline.py
+++ pipeline.py
@@ -6,7 +6,29 @@ client = boto3.client('codebuild')
 # projects = client.list_projects()
 # print(projects)
 
-print('WORKFLOW', os.environ['WORKFLOW'])
-print('BRANCH', os.environ['BRANCH'])
-print('TEST', os.environ['TEST'])
-print('GIT_URL', os.environ['GIT_URL'])
+git_url = os.environ['GIT_URL']
+site_git = git_url.split('/')[-1]
+site = site_git.split('.')[0]
+print(site)
+
+# todo: Use build environment
+env_var = os.environ['ENV']
+print(env_var)
+
+response = client.start_build(
+    projectName='nu-ecsplatform-orchestrator',
+    environmentVariablesOverride=[
+        {
+            'name': 'WORKFLOW',
+            'value': os.environ['WORKFLOW']
+        },
+        {
+            'name': 'SITE',
+            'value': site
+        },
+        {
+            'name': 'SITE_BRANCH',
+            'value': os.environ['BRANCH']
+        },
+    ]
+)
